86203353daa086307ee7f90e1cefd45fc54e2410
Dmytro <kotykhin_d@ukr.net>
2022-09-17 16:30:20 +0300

    api delete  autorization

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import { GetStaticProps, NextPage } from 'next';
+import { GetServerSideProps, GetStaticProps, NextPage } from 'next';
 
 import NavDrawer from "../components/drawer/Drawer";
 import FirstBlock from "../components/firstBlock/FirstBlock";
@@ -49,7 +49,7 @@ const Home: NextPage<ICatalog> = ({ cataloglist }) => {
 
 export default Home;
 
-export const getStaticProps: GetStaticProps = async () => {
+export const getServerSideProps: GetServerSideProps = async () => {
     const data = await GetAllList()
     return {
         props: {
