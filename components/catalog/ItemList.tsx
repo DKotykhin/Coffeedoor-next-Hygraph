6df7d08e21dedc53827eb21b045dc85884d82346
Dmytro <kotykhin_d@ukr.net>
2022-09-30 22:34:30 +0300

    change card slider

--- components/catalog/ItemList.tsx
+++ components/catalog/ItemList.tsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
 import { useRouter } from "next/router";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation } from "swiper";
+import { EffectCards, Navigation } from "swiper";
 import { motion, AnimatePresence } from "framer-motion";
 
-import { Container, Typography } from "@mui/material";
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { Box, Container, Typography } from "@mui/material";
 
 import Card from "../card/Card";
 import FilterItems from "../filters/FilterItems";
@@ -19,19 +20,20 @@ interface IItemList {
     itemList: ICard[];
     id: string;
     title: string;
-    subtitle: string;    
-    filterArray?: IFilter[]    
+    subtitle: string;
+    filterArray?: IFilter[]
 }
 
 const ItemList: React.FC<IItemList> = ({
     itemList,
     id,
     title,
-    subtitle,    
-    filterArray       
+    subtitle,
+    filterArray
 }) => {
     const [list, setList] = useState(itemList);
     const router = useRouter();
+    const matches = useMediaQuery('(min-width:1200px)');
 
     const onSelectSort = (sort: string) => {
         let data = itemList;
@@ -62,38 +64,52 @@ const ItemList: React.FC<IItemList> = ({
                     filterArray={filterArray}
                 />
             )}
-            <Swiper
-                slidesPerView={1.2}
-                spaceBetween={10}                
-                breakpoints={{
-                    850: {
-                        slidesPerView: 2.2,
-                        threshold: 20
-                    },
-                    1200: {
-                        slidesPerView: 3,
-                        threshold: 20
-                    },
-                }}                
-                navigation={true}
-                modules={[Navigation]}
-                grabCursor={true}                
-            >
-                <AnimatePresence initial={false}>
-                    {list?.map((item) => (
-                        <SwiperSlide key={item.node.id}>
-                            <motion.div
-                                initial={{ opacity: 0 }}
-                                animate={{ opacity: 1 }}
-                                exit={{ opacity: 0 }}
+            {matches ?
+                list?.map((item) => (
+                    <Box key={item.node.id} sx={{ display: "inline-block" }}>
+                        <Card item={item} lang={router.locale} />
+                    </Box>
+                ))
+                :
+                <Swiper
+                    slidesPerView={1.2}
+                    spaceBetween={10}
+                    // breakpoints={{
+                    //     850: {
+                    //         slidesPerView: 2.2,
+                    //         threshold: 20
+                    //     },
+                    //     1200: {
+                    //         slidesPerView: 3,
+                    //         threshold: 20
+                    //     },
+                    // }}
+                    // navigation={true}
+                    // modules={[Navigation, EffectCards]}
+                    modules={[EffectCards]}
+                    grabCursor={true}
+                    effect={"cards"}
+                    cardsEffect={{
+                        slideShadows: false
+                    }}
+                    centeredSlides={true}
+                >
+                    <AnimatePresence initial={false}>
+                        {list?.map((item) => (
+                            <SwiperSlide key={item.node.id}>
+                                <motion.div
+                                    initial={{ opacity: 0 }}
+                                    animate={{ opacity: 1 }}
+                                    exit={{ opacity: 0 }}
                                 // transition={{duration: 0.5}}
-                            >
-                                <Card item={item} lang={router.locale} />
-                            </motion.div>
-                        </SwiperSlide>
-                    ))}
-                </AnimatePresence>
-            </Swiper>
+                                >
+                                    <Card item={item} lang={router.locale} />
+                                </motion.div>
+                            </SwiperSlide>
+                        ))}
+                    </AnimatePresence>
+                </Swiper>
+            }
         </Container>
     );
 };
